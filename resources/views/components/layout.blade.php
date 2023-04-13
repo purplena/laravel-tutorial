b3839e14987d0c926b9ec2bd03ab1f3820faae7a
= <=>
2023-04-13 14:08:47 +0200

    Code after Section 11: Newsletters and APIs

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,6 +11,12 @@
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </head>
 
+<style>
+    html {
+        scroll-behavior: smooth;
+    }
+</style>
+
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
         <nav class="md:flex md:justify-between md:items-center max-w-6xl mx-auto">
@@ -32,7 +38,7 @@
                     <a href="/register" class="text-xs font-bold uppercase">Register</a>
                     <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
                 @endguest
-                <a href="#"
+                <a href="#newsletter"
                     class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">Subscribe
                     for Updates</a>
             </div>
@@ -40,20 +46,23 @@
 
         {{ $slot }}
 
-        <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+        <footer id="newsletter"
+            class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
             <img src="/images/lary-newsletter-icon.png" alt="Avatar" class="mx-auto" style="width: 145px" />
             <h5 class="text-3xl">Stay in touch with the latest posts</h5>
             <p class="text-sm">Promise to keep the inbox clean. No bugs.</p>
 
             <div class="mt-10">
                 <div class="relative inline-block mx-auto bg-gray-200 rounded-full">
-                    <form method="post" action="/" class="flex text-sm">
+                    <form method="post" action="/newsletter" class="flex text-sm">
+                        @csrf
                         <div class="py-3 px-5 inline-flex items-center">
                             <label for="email" class="hidden md:inline-block">
                                 <img src="/images/mailbox-icon.svg" alt="letter icon" />
                             </label>
-                            <input type="text" placeholder="Your email address"
+                            <input id='email' name="email" type="text" placeholder="Your email address"
                                 class="bg-transparent pl-4 focus-within:outline-none" />
+
                         </div>
                         <button type="submit"
                             class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8 hover:bg-blue-700">
@@ -62,6 +71,9 @@
                     </form>
                 </div>
             </div>
+            @error('email')
+                <span class="text-xs text-red-500">{{ $message }}</span>
+            @enderror
         </footer>
     </section>
     <x-flash />
--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,14 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsletterController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
